1eff1cd6811b937df61d842da29166c364777572
Borja <borja@i2b.studio>
2024-01-10 19:21:38 +0100

    chore: Add Hilt dependency 2.45
    https://developer.android.com/training/dependency-injection/hilt-android

--- app/src/main/java/com/architecture/user/MainActivity.kt
+++ app/src/main/java/com/architecture/user/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.architecture.user.ui.theme.UserTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/architecture/user/UserApplication.kt
@@ -0,0 +1,8 @@
+package com.architecture.user
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class UserApplication: Application() {
+}
