7970cfcb677fa4353c789b3568fc0ffc1e031d8c
Borja <borja@i2b.studio>
2024-01-25 17:53:35 +0100

    test: UiState in Compose

--- feature-users/src/androidTest/java/com/architecture/feature_users/list/UserListScreenKtTest.kt
+++ feature-users/src/androidTest/java/com/architecture/feature_users/list/UserListScreenKtTest.kt
@@ -2,8 +2,10 @@ package com.architecture.feature_users.list
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import com.architecture.core.model.User
+import com.architecture.core.state.UiState
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,6 +23,31 @@ class UserListScreenKtTest {
         }
     }
 
+    @Test
+    fun displaysUserMainViewUserList() {
+        with(composeTestRule) {
+            setContent { UserMainView(state = uiStateSuccess, {}, {}) }
+            onNodeWithText("Laura").assertIsDisplayed()
+            onNodeWithText("laura@gmail.com").assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun displaysUserMainViewLoading() {
+        with(composeTestRule) {
+            setContent { UserMainView(state = uiStateLoading, {}, {}) }
+            onNodeWithTag("CircularProgressIndicator").assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun displaysUserMainViewError() {
+        with(composeTestRule) {
+            setContent { UserMainView(state = uiStateError, {}, {}) }
+            onNodeWithText("No internet connection").assertIsDisplayed()
+        }
+    }
+
 }
 
 private val testUserItem = User(
@@ -29,3 +56,6 @@ private val testUserItem = User(
     picture = ""
 )
 
+private val uiStateSuccess = UiState.Success(listOf(testUserItem))
+private val uiStateLoading = UiState.Loading
+private val uiStateError = UiState.Error.NoConnection("No internet connection")
