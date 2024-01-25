1d9df4e309443eabe6e8fa9a4365dded9afc8b42
Borja <borja@i2b.studio>
2024-01-25 16:58:48 +0100

    Create a BaseViewModel
    This abstract ViewModel is used as the base for the rest of
    the ViewModels.
    In this way we could reuse the common logic in all of them
    and avoid boilerplate code.

--- feature-users/src/main/java/com/architecture/feature_users/list/UserViewModel.kt
+++ feature-users/src/main/java/com/architecture/feature_users/list/UserViewModel.kt
@@ -1,11 +1,11 @@
 package com.architecture.feature_users.list
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.architecture.core.model.User
 import com.architecture.core.navigation.NavRoutes
 import com.architecture.core.navigation.UserInput
 import com.architecture.core.repository.UserRepository
+import com.architecture.core.state.BaseViewModel
 import com.architecture.core.state.DataState
 import com.architecture.core.state.UiState
 import com.architecture.core.state.asUiState
@@ -13,40 +13,22 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import javax.inject.Inject
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class UserViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
-
-    private val _uiStateFlow: MutableStateFlow<UiState<List<User>>> = MutableStateFlow(UiState.Loading)
-    val uiStateFlow: StateFlow<UiState<List<User>>> = _uiStateFlow
-    private val actionFlow: MutableSharedFlow<UserUiAction> = MutableSharedFlow()
-    private val _singleEventFlow = Channel<UserListUiSingleEvent>()
-    val singleEventFlow = _singleEventFlow.receiveAsFlow()
+class UserViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : BaseViewModel<List<User>, UiState<List<User>>, UserUiAction, UserListUiSingleEvent>() {
 
     private var userListToSearch = listOf<User>()
 
+    override fun initState(): UiState<List<User>> = UiState.Loading
+
     init {
-        viewModelScope.launch {
-            actionFlow.collect {
-                handleAction(it)
-            }
-        }
         submitAction(UserUiAction.Load)
     }
 
-    fun submitAction(action: UserUiAction) {
-        viewModelScope.launch {
-            actionFlow.emit(action)
-        }
-    }
-
-    private fun handleAction(action: UserUiAction) {
+    override fun handleAction(action: UserUiAction) {
         when (action) {
             is UserUiAction.Load -> {
                 loadUserList()
@@ -76,15 +58,9 @@ class UserViewModel @Inject constructor(private val userRepository: UserReposito
         var result = listOf<User>()
         if (userListToSearch.isNotEmpty()) {
             result = userListToSearch
-                .filter { it.name.contains(query, ignoreCase = true)  || it.email.contains(query, ignoreCase = true) }
+                .filter { it.name.contains(query, ignoreCase = true) || it.email.contains(query, ignoreCase = true) }
         }
         submitState(UiState.Success(result))
-    }
-
-    private fun submitState(state: UiState<List<User>>) {
-        viewModelScope.launch {
-            _uiStateFlow.value = state
-        }
     }
 
     private fun loadUserList() {
@@ -96,15 +72,10 @@ class UserViewModel @Inject constructor(private val userRepository: UserReposito
                         userListToSearch = it.data
                         submitState(UiState.Success(it.data))
                     }
+
                     is DataState.Error -> submitState(it.asUiState())
                 }
             }
-        }
-    }
-
-    private fun submitSingleEvent(event: UserListUiSingleEvent) {
-        viewModelScope.launch {
-            _singleEventFlow.send(event)
         }
     }
 
--- feature-users/src/main/java/com/architecture/feature_users/single/UserSingleViewModel.kt
+++ feature-users/src/main/java/com/architecture/feature_users/single/UserSingleViewModel.kt
@@ -1,48 +1,22 @@
 package com.architecture.feature_users.single
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.architecture.core.model.User
+import com.architecture.core.state.BaseViewModel
 import com.architecture.core.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 @HiltViewModel
-class UserSingleViewModel @Inject constructor() :  ViewModel() {
+class UserSingleViewModel @Inject constructor() :
+    BaseViewModel<User, UiState<User>, UserSingleUiAction, Nothing>() {
 
-    private val _uiStateFlow: MutableStateFlow<UiState<User>> = MutableStateFlow(UiState.Loading)
-    val uiStateFlow: StateFlow<UiState<User>> = _uiStateFlow
-    private val actionFlow: MutableSharedFlow<UserSingleUiAction> = MutableSharedFlow()
+    override fun initState(): UiState<User> = UiState.Loading
 
-    init {
-        viewModelScope.launch {
-            actionFlow.collect {
-                handleAction(it)
-            }
-        }
-    }
-
-    fun submitAction(action: UserSingleUiAction) {
-        viewModelScope.launch {
-            actionFlow.emit(action)
-        }
-    }
-
-    private fun handleAction(action: UserSingleUiAction) {
+    override fun handleAction(action: UserSingleUiAction) {
         when (action) {
             is UserSingleUiAction.Load -> {
                 loadUser(action.userName, action.email, action.picture)
             }
-        }
-    }
-
-    private fun submitState(state: UiState<User>) {
-        viewModelScope.launch {
-            _uiStateFlow.value = state
         }
     }
 
